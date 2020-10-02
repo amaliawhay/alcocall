7c1c664856f069d5818eb437a07b95767c90d89f
Amalia Hay <amaliawhay@gmail.com>
2020-10-01 21:02:58 -0700

    API calls

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -3,35 +3,42 @@ import axios from "axios";
 export default {
   getVodka: function () {
     return axios.get(
-      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka"
+      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=vodka"
     );
   },
   getWhiskey: function () {
     return axios.get(
-      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=whiskey"
+      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=whiskey"
     );
   },
   getGin: function () {
     return axios.get(
-      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin"
+      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=gin"
     );
   },
   getRum: function () {
     return axios.get(
-      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=rum"
+      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=rum"
     );
   },
   getTequila: function () {
     return axios.get(
-      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=tequila"
+      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=tequila"
     );
   },
   getNonAlcohol: function () {
     return axios.get(
-      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?a=Non_Alcoholic"
     );
   },
   getRandom: function () {
-    return axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
-  }
+    return axios.get(
+      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/random.php"
+    );
+  },
+  getMultiIng: function () {
+    return axios.get(
+      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=Dry_Vermouth,Gin,Anis"
+    );
+  },
 };
