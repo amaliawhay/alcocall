55f9150f45cd08f95469d294fdb45fd14bcd23e3
Amalia Hay <amaliawhay@gmail.com>
2020-10-03 10:30:43 -0700

    Added redux. Cleaned up API calls

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,31 +1,6 @@
 import axios from "axios";
 
 export default {
-  getVodka: function () {
-    return axios.get(
-      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=vodka"
-    );
-  },
-  getWhiskey: function () {
-    return axios.get(
-      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=whiskey"
-    );
-  },
-  getGin: function () {
-    return axios.get(
-      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=gin"
-    );
-  },
-  getRum: function () {
-    return axios.get(
-      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=rum"
-    );
-  },
-  getTequila: function () {
-    return axios.get(
-      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=tequila"
-    );
-  },
   getNonAlcohol: function () {
     return axios.get(
       "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?a=Non_Alcoholic"
@@ -36,9 +11,9 @@ export default {
       "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/random.php"
     );
   },
-  getMultiIng: function () {
+  getIngs: function () {
     return axios.get(
-      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i=Dry_Vermouth,Gin,Anis"
+      "https://www.thecocktaildb.com/api/json/{REACT_APP_COCKTAILS_API_KEY}/filter.php?i="
     );
   },
 };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var compression = require("compression");
 var express = require("express");
 require("dotenv").config();
 
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
