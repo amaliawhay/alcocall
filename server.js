93da5934c3da79cf15ccf7acf64b976af7ade7fd
Ian Martinez <imartinez0753@gmail.com>
2020-10-03 11:31:38 -0700

    backend troubleshooting

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var compression = require("compression");
 var express = require("express");
 require("dotenv").config();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
 var app = express();
 
@@ -14,9 +14,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Import routes and give the server access to them.
-//var routes = require("./controllers/controller.js");
+var routes = require("./controllers/controller.js");
 
-//app.use(routes);
+app.use(routes);
 
 app.listen(PORT, function () {
   console.log("App now listening at localhost:" + PORT);
