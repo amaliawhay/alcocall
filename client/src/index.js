6368e25c9a7b873ec431df703376ab89c1c79acc
Amalia Hay <amaliawhay@gmail.com>
2020-10-04 12:49:44 -0700

    Removed react app

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "materialize-css";
-import "materialize-css/dist/css/materialize.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
