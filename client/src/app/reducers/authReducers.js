d4883f0b7b5dd7b3139f7bcf48dfed5571cbc0ef
Amalia Hay <amaliawhay@gmail.com>
2020-10-15 20:07:10 -0700

    authReducer.js updated

--- client/src/app/reducers/authReducers.js
+++ client/src/app/reducers/authReducers.js
@@ -10,3 +10,22 @@ const initialState = {
   user: {},
   loading: false,
 };
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload,
+      };
+    case USER_LOADING:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+}
