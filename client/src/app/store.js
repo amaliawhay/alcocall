0274b34b9b9e0ae4b0dcf682cfb208da21c464ff
Amalia Hay <amaliawhay@gmail.com>
2020-10-15 21:17:24 -0700

    Set up root reducer in store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -4,13 +4,14 @@ import {
   compose,
 } from "redux";
 import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-  () => [],
+  rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
