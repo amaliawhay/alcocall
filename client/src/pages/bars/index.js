a28d921fa299ddb139006779b8889d4908cf0742
Yulin Karaiscos <ykaraiscos@yahoo.com>
2020-10-17 12:10:46 -0700

    Fixed Bars and breweries styling and content

--- client/src/pages/bars/index.js
+++ client/src/pages/bars/index.js
@@ -2,9 +2,33 @@ import React from "react";
 
 function bars() {
   return (
-    <div>
-      <h1>bars</h1>
-    </div>
+    <main className="container main-content">
+      <div className="col s12 l6">
+        <div className="card">
+          <div className="card-image">
+            {/* Google Maps API Embedded */}
+            <iframe src="https://www.google.com/maps/embed/v1/search?q=bars%20near%20me&amp;key=AIzaSyAO-aBYWPDQ12tw7JzsIlrdQk8ZDIcuWho" style={{border: 0}} allowfullscreen="" aria-hidden="false" tabindex="0" width="100%" height="400px" frameborder="0"></iframe>
+            <a className="halfway-fab btn-floating light-blue darken-1 pulse" href="">
+              <i className="material-icons">location_on</i>
+            </a>
+          </div>
+          <div className="card-content">
+            <span className="card-title light-blue-text text-darken-1">Bar Locater</span>
+            <p>Get Bar Hopping!</p>
+          </div>
+          <div className="card-action center-align">
+          <a class="btn light-blue darken-1 card-buttons" href="https://www.google.com/search?q=bars+near+me&amp;oq=bars+near+me&amp;aqs=chrome..69i57.1608j0j7&amp;sourceid=chrome&amp;ie=UTF-8">Find a local bar</a>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <h5 className="light-blue-text text-darken-1">Please Drink Responsibly!</h5>
+        <p>
+          Have you already started drinking? Call a rideshare app or a friend to
+          take you on the next journey of your adventure!
+        </p>
+      </div>
+    </main>  
   );
 }
 export default bars;
--- client/src/pages/breweries/index.js
+++ client/src/pages/breweries/index.js
@@ -2,9 +2,24 @@ import React from "react";
 
 function breweries() {
   return (
-    <div>
-      <h1>breweries</h1>
-    </div>
+    <main className="container main-content">
+     <div className="col s12 l6">
+      <div className="card">
+        <h5 className="card-content light-blue-text text-darken-1">Enter a city name to find the closest brewery near you.</h5>
+          <form className="row card-content">
+           <div className="input-field col s12 l9">
+            <input id="firstName" class="validate" placeholder="City Name" type="text"></input>
+            <label className="active" htmlFor="city_name">
+              City Name
+            </label>
+            </div>
+          </form>
+          <a id="button" className="col s8 l3 center btn:hover btn light-blue darken-1">Find Local Beer!
+          </a>
+          <div id="brewery"></div>
+      </div>    
+      </div>
+    </main>  
   );
 }
 export default breweries;
