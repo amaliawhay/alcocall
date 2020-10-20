74c794d3457484fa87bbb5c8b710dd39ef97db1a
Amalia Hay <amaliawhay@gmail.com>
2020-10-19 19:46:02 -0700

    Build dir

---
+++ client/build/precache-manifest.11445f87b2400a0598c1f33f53162891.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b784f8f0171bfbd82b026ad754eee34a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63dd6bf9263056fc87f0",
+    "url": "/static/css/2.c7b178aa.chunk.css"
+  },
+  {
+    "revision": "2d021c9760f5a9f90d49",
+    "url": "/static/css/main.b8c2bcc1.chunk.css"
+  },
+  {
+    "revision": "63dd6bf9263056fc87f0",
+    "url": "/static/js/2.d37a1587.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/static/js/2.d37a1587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d021c9760f5a9f90d49",
+    "url": "/static/js/main.65fd1166.chunk.js"
+  },
+  {
+    "revision": "1a34d4dd367f2158c388",
+    "url": "/static/js/runtime-main.f3148320.js"
+  }
+]);
