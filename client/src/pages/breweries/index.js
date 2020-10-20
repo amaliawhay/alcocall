c15dcef7ba8da8e50cd541a4057c7bf87ae44277
Ian Martinez <imartinez0753@gmail.com>
2020-10-20 14:08:53 -0700

    style update

--- client/src/pages/breweries/index.js
+++ client/src/pages/breweries/index.js
@@ -1,33 +1,35 @@
 import React from "react";
-
+ 
 function breweries() {
-  return (
-    <main className="container main-content">
-     <div className="col s12 l6">
-      <div className="card card-content">
-        <h4 className="h4 card-content light-blue-text text-darken-1">Enter a city name to find the closest brewery near you.</h4>
-          <form className="row card-content">
-           <div className="input-field col s12 l9">
-            <input id="firstName" class="validate" placeholder="City Name" type="text"></input>
-            <label className="active label-city" htmlFor="city_name">
-              City Name
-            </label>
-            </div>
-          </form>
-          <div className="card-action center-align">
-          <a id="button" class="btn light-blue darken-1 card-buttons">Find Local Beer!</a>
-          </div>
-          <div id="brewery"></div>
-      </div>    
-      </div>
-      <div className="">
-        <h4 className="center-align light-blue-text text-darken-1">Please Drink Responsibly!</h4>
-        <p className="center-align">
-          Have you already started drinking? Call a rideshare app or a friend to
-          take you on the next journey of your adventure!
-        </p>
-      </div>
-    </main>  
-  );
+ return (
+   <main className="container main-content">
+    <div className="col s12 l6">
+     <div className="card card-content">
+       <h4 className="brewery-header card-content light-blue-text text-darken-1">Enter a city name to find the closest brewery near you.</h4>
+         <form className="row card-content">
+          <div className="input-field col s12 l9">
+           <input id="firstName" class="validate" placeholder="City Name" type="text"></input>
+           <label className="active label-city" htmlFor="city_name">
+             City Name
+           </label>
+           </div>
+         </form>
+         <div className="card-action center-align">
+         <a id="button" class="btn light-blue darken-1 card-buttons">Find Local Beer!</a>
+         </div>
+         <div id="brewery"></div>
+     </div>   
+     </div>
+     <div className="">
+       <h4 className="center-align light-blue-text text-darken-1">Please Drink Responsibly!</h4>
+       <p className="center-align">
+         Have you already started drinking? Call a rideshare app or a friend to
+         take you on the next journey of your adventure!
+       </p>
+     </div>
+   </main> 
+ );
 }
 export default breweries;
+ 
+
--- client/src/pages/references/index.js
+++ client/src/pages/references/index.js
@@ -1,58 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+ 
 function references() {
-  return (
-    <div>
-      <main className="container main-content">
-        <h4 className="h4">Want to continue your Alcoholic Education?</h4>
-        <h5 className="light-blue-text text-darken-1">
-          Here are some outside resources...
-        </h5>
-        <ul>
-          <Link
-            className="resource-a"
-            to="https://www.thespruceeats.com/bartending-tips-for-better-drinks-4105954"
-          >
-            Bartending 101: The Spruce Eats
-          </Link>
-        </ul>
-        <ul>
-          <Link
-            className="resource-a"
-            to="https://www.nationalgeographic.com/magazine/2017/02/alcohol-discovery-addiction-booze-human-culture/"
-          >
-            "Our 9,000 Year Love Affair With Booze" by NatGeo
-          </Link>
-        </ul>
-        <ul>
-          <Link
-            className="resource-a"
-            to="http://alcohol-facts.net/Alcohol-History-Facts.html"
-          >
-            Alcohol History Facts
-          </Link>
-        </ul>
-
-        <h5 className="light-blue-text text-darken-1">
-          And we hope you are drinking responsibly, but remember, Help is always
-          available!
-        </h5>
-        <ul>
-          <Link className="resource-a" to="https://www.niaaa.nih.gov">
-            The National Institute on Alcoholism and Alcohol Abuse
-          </Link>{" "}
-        </ul>
-        <ul>
-          <Link
-            className="resource-a"
-            to="https://www.aa.org/pages/en_US/find-aa-resources"
-          >
-            Alcoholics Anonymous
-          </Link>{" "}
-        </ul>
-      </main>
-    </div>
-  );
+ return (
+   <div>
+     <main className="container main-content">
+       <h4 className="brewery-header">Want to continue your Alcoholic Education?</h4>
+       <h5 className="light-blue-text text-darken-1">
+         Here are some outside resources...
+       </h5>
+       <ul>
+         <Link
+           className="resource-a"
+           to="https://www.thespruceeats.com/bartending-tips-for-better-drinks-4105954"
+         >
+           Bartending 101: The Spruce Eats
+         </Link>
+       </ul>
+       <ul>
+         <Link
+           className="resource-a"
+           to="https://www.nationalgeographic.com/magazine/2017/02/alcohol-discovery-addiction-booze-human-culture/"
+         >
+           "Our 9,000 Year Love Affair With Booze" by NatGeo
+         </Link>
+       </ul>
+       <ul>
+         <Link
+           className="resource-a"
+           to="http://alcohol-facts.net/Alcohol-History-Facts.html"
+         >
+           Alcohol History Facts
+         </Link>
+       </ul>
+ 
+       <h5 className="light-blue-text text-darken-1">
+         And we hope you are drinking responsibly, but remember, Help is always
+         available!
+       </h5>
+       <ul>
+         <Link className="resource-a" to="https://www.niaaa.nih.gov">
+           The National Institute on Alcoholism and Alcohol Abuse
+         </Link>{" "}
+       </ul>
+       <ul>
+         <Link
+           className="resource-a"
+           to="https://www.aa.org/pages/en_US/find-aa-resources"
+         >
+           Alcoholics Anonymous
+         </Link>{" "}
+       </ul>
+     </main>
+   </div>
+ );
 }
 export default references;
+ 
+
