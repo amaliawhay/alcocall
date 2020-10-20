76a702358accdb35b8b3040d8ed3ed22ff3c6044
Ian Martinez <imartinez0753@gmail.com>
2020-10-20 12:26:55 -0700

    trying crossorigin.me

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+
 export default {
   getNonAlcohol: function () {
     return axios.get(
-      `https://www.thecocktaildb.com/api/json/${process.env.REACT_APP_COCKTAILS_API_KEY}/filter.php?a=Non_Alcoholic`
+      `https://crossorigin.me/https://www.thecocktaildb.com/api/json/${process.env.REACT_APP_COCKTAILS_API_KEY}/filter.php?a=Non_Alcoholic`
     );
   },
   getRandom: function () {
